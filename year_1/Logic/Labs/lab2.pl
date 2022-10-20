16bdbb9320ca7d100b20103870bb392cb9cd61b7
MatthewAlgo <matthewdy0101@gmail.com>
2022-10-20 23:35:41 +0300

    Updated prolog lab2.pl

--- year_1/Logic/Labs/lab2.pl
+++ year_1/Logic/Labs/lab2.pl
@@ -1,5 +1,11 @@
-distance((X1, Y1), (X2, Y2), C) :- (C is sqrt((X1 - X2)**2 + (Y1-Y2)**2)).
-add(X, Y, Z) :- Z is X+Y.
+% Student exercise profile
+:- set_prolog_flag(occurs_check, error).        % disallow cyclic terms
+:- set_prolog_stack(global, limit(8 000 000)).  % limit term space (8Mb)
+:- set_prolog_stack(local,  limit(2 000 000)).  % limit environment space
+
+% Your program goes here
+
+distance((A, B), (C, D), X):- X is sqrt((A-C) ** 2 + (B-D) ** 2).
 
 parent(bob, lisa).
 parent(bob, paul).
@@ -12,8 +18,22 @@ parent(lisa, harry).
 parent(mary, dony).
 parent(mary, sandra).
 
-fib(0, 1, 1).
-fib(N, X, Y) :- N > 0, N1 is N-1, fib(N1, A, B), X is B, Y is A+B.
+ancestor_of(X, Y) :- parent(X, Y).
+ancestor_of(X, Y) :- parent(X, Z), ancestor_of(Z, Y).
+
+fib(0,1). % fib(0, 1) is 1
+fib(1,1). % fib(1, 1) is 1
+fib(N,X) :- 2 =< N, M is N - 1, fib(M, Y), P is N - 2, fib(P, Z), X is Y + Z.
+% fib(N, X) adevarat daca n >= 2 si M este n-1 si fib(M, Y) si P este n-2, fib(P, Z) si X este Y+Z
+% adaugam fib(n-1) cu fib(n-2)
+
+fibo(0,0,1). % fibo(0,0,1) este 1
+fibo(1,1,1). % fibo(1,1,1) este 1
+fibo(N,Z,X) :- 2 =< N, M is N-1, fibo(M,Y,Z), X is Y + Z.
+% N>=2 si M este N-1 si fibo(M, Y, Z) si X = Y+Z
+
+fibg(N,X) :- fibo(N,_,X).
+
 
 lg([], 0).
 lg([_|T], X) :- lg(T, Y), X is Y+1.
@@ -25,3 +45,8 @@ square(X, Y1, C) :- Y1 >=1, line(Y, C), A is Y1-1, square(Y, A, C).
 
 element of(X,[X|_]).
 element of(X,[_|Tail]) :- element of(X,Tail).
+
+/** <examples> Your example queries go here, e.g.
+?- member(X, [cat, mouse]).
+*/
+
